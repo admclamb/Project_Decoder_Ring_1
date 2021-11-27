8c93eb27b14431f33288c8860833ba649e247317
admclamb <adylanmclamb@yahoo.com>
2021-11-26 20:49:50 -0500

    commit on 'main'

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,12 @@
-// Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution.js");
+
+describe("substitution", () => {
+  it("Return false if alphabet is missing", () => {
+    const input = "hello";
+    const alphabet = undefined;
+    const actual = substitution(input, alphabet, true);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  });
+});
